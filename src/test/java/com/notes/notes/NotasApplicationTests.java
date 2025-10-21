af9e2b07568c3a113cceda0b83d6387a02cad5d7
Andrea <andrea_1996_melli@hotmail.com>
2025-10-21 18:13:31 +0200

    test

--- src/test/java/com/notes/notes/NotasApplicationTests.java
+++ src/test/java/com/notes/notes/NotasApplicationTests.java
@@ -3,7 +3,7 @@ package com.notes.notes;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+  @SpringBootTest
 class NotasApplicationTests {
 
 	@Test
---
+++ src/test/java/com/notes/notes/TestNoteService.java
@@ -0,0 +1,55 @@
+/* package com.notes.notes;
+
+import com.notes.notes.model.Note;
+import com.notes.notes.repository.NoteRepository;
+import com.notes.notes.service.NoteService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class TestNoteService {
+    @Mock
+    private NoteRepository noteRepository;
+
+    @InjectMocks
+    private NoteService noteService;
+
+    @Test
+    void obteinAllNotes() {
+        //Arrange (Preparar)
+        Note note1 = new Note();
+        note1.setDescription("Descripcion 1");
+        Note note2 = new Note();
+        note2.setDescription("Descripcion 2");
+
+        List<Note> testNote = Arrays.asList(note1, note2);
+
+        when (noteRepository.findAll()).thenReturn((testNote));
+
+        // Act (Actuar)
+        List<Note> result = noteService.obtainAll();
+
+        // Assert (Verificar / comparar rdo esperado rdo obtenido)
+
+        assertEquals(2, result.size());
+        assertEquals("Description 1", result.get(0).getDescription());
+        assertEquals("Description 2", result.get(1).getDescription());
+
+
+
+    }
+
+
+
+}
+
+*/
