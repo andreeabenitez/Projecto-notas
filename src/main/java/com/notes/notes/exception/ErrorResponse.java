bfff36b440fc84175bef3230ff9b50b8d3de31c5
Andrea <andrea_1996_melli@hotmail.com>
2025-10-28 17:47:37 +0100

    fix: ErrorResponse

--- src/main/java/com/notes/notes/exception/ErrorResponse.java
+++ src/main/java/com/notes/notes/exception/ErrorResponse.java
@@ -8,6 +8,13 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 public class ErrorResponse {
     private int status;
     private String message;
+    private long timestamp;
+
+    public ErrorResponse(int status, String message, long timestamp) {
+        this.status = status;
+        this.message = message;
+        this.timestamp = timestamp;
+    }
 
     public long getTimestamp() {
         return timestamp;
@@ -34,30 +41,28 @@ public class ErrorResponse {
     }
 
 
-//OMITO GETTER Y SETTER
-
-
     @ControllerAdvice
     public class GlobalExceptionHandler {
 
         // Maneja la excepción cuando una tarea no se encuentra
-        @ExceptionHandler(TareaNotFoundException.class)
-        public ResponseEntity<ErrorResponse> handleTareaNotFound(TareaNotFoundException ex) {
+        @ExceptionHandler(NoteNotFoundException.class)
+        public ResponseEntity<ErrorResponse> handleNoteNotFound(NoteNotFoundException ex) {
             // Código 404 (Not Found)
             HttpStatus status = HttpStatus.NOT_FOUND;
 
             // 1. Crea el objeto ErrorResponse
-            ErrorResponse error = new ErrorResponse(status.value(), ex.getMessage());
+            ErrorResponse error = new ErrorResponse(status.value(), ex.getMessage(), getTimestamp());
 
             // 2. Devuelve ResponseEntity, que convierte 'error' a JSON y establece el 404
             return new ResponseEntity<>(error, status);
         }
     }
 
-    public class TareaNotFoundException extends RuntimeException {
+    public class NoteNotFoundException extends RuntimeException {
 
         // Constructor
-        public TareaNotFoundException(Long id) {
+        public NoteNotFoundException(Long id) {
             super("No se pudo encontrar la tarea con ID: " + id);
         }
     }
+}
